ba2e4ad148a16b78b2fd477b9e3c48da198bfea5
fde-jesu <Fernando Lucart>
2025-08-05 16:16:27 +0100

    done

--- ex01/Iter.hpp
+++ ex01/Iter.hpp
@@ -4,12 +4,9 @@
 #include <cstring>
 
 
-template <typename T1>
-void iter(T1 *arr, size_t size, void (*f)(T1))
+template <typename T1, typename T2>
+void iter(T1 *arr, int size, void (*f)(T2 &))
 {
     for (size_t i = 0; i < size; i++)
-    {
-        *f(arr[i]);
-    }
-    
-}
+        f(arr[i]);
+} 
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,14 +1,6 @@
 #include "Iter.hpp"
 
-template <typename T1>
-void iter(T1 *arr, size_t size, void (*f)(T1&))
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        f(arr[i]);
-    }
-    
-}
+
 
 void _add(int &nbr);
 void reverse(char &letter);
@@ -26,47 +18,20 @@ void reverse(char &a)
     else if (a >= 'a' && a <= 'z')
         a-=32;
    // std::cout << a << std::endl;
-    
-    
 }
 
-
-int main()
+ int main()
 {
-    int arr1[5] = {1,2,3,4,5}; 
-    char  arr2[5] = {'a','b','c','d','e'}; 
+    int arr1[] = {1,2,3,4,5}; 
+    char  arr2[] = {'a','b','c','d','e'}; 
 
     iter(arr1, 5, _add);
     iter(arr2, 5, reverse);
+    for (int i = 0; i < 5; i++)
+        std::cout << arr1[i] << std::endl;
+    std::cout << std::endl;
+        for (int i = 0; i < 5; i++)
+        std::cout << arr2[i] << std::endl;
+    
     return 0;
 }
-/* 
-class Awesome
-{
-  public:
-    Awesome( void ) : _n( 42 ) { return; }
-    int get( void ) const { return this->_n; }
-  private:
-    int _n;
-};
-
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
-{
-  o << rhs.get();
-  return o;
-}
-
-template< typename T >
-void print( T& x )
-{
-  std::cout << x << std::endl;
-  return;
-}
-
-int main() {
-  int tab[] = { 0, 1, 2, 3, 4 };
-  Awesome tab2[5];
-
-  iter( tab, 5, print<const int> );
-  iter( tab2, 5, print<Awesome> );
- */
