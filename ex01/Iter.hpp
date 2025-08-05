8f66fda51ba042fc20bf5779ac96fe9d6aa3cde2
fde-jesu <Fernando Lucart>
2025-08-05 16:21:54 +0100

    done

--- ex01/Iter.hpp
+++ ex01/Iter.hpp
@@ -5,7 +5,7 @@
 
 
 template <typename T1, typename T2>
-void iter(T1 *arr, int size, void (*f)(T2 &))
+void iter(T1 *arr, size_t size, void (*f)(T2 &))
 {
     for (size_t i = 0; i < size; i++)
         f(arr[i]);
